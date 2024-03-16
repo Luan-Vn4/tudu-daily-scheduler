73308e01b84160e3355f8672e1635e57cd3d71bd
Paula Beatriz <paulinhabeatrizcontato@gmail.com>
2024-03-16 15:16:19 -0300

    melhora na todo list, adicao da funcao de adicionar, remover e editar uma task

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -8,21 +8,40 @@ function addTask(){
     else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
+        span.className = "delete";
+        span.onclick = function() {
+            let div = this.parentElement;
+            div.style.display = "none";
+            saveData();
+        };
         li.appendChild(span);
+        
+        let editButton = document.createElement("button");
+        editButton.innerHTML = "Edit";
+        editButton.classList.add("edit-button");
+        editButton.onclick = function() {
+        let newText = prompt("Edit your task:", li.firstChild.textContent);
+        if (newText !== null && newText.trim() !== '') {
+            li.firstChild.textContent = newText;
+            saveData();
+        }
+        };
+        li.appendChild(editButton);
 
+
+        listContainer.appendChild(li);
     }
     inputBox.value = "";
     saveData();
-
 }
-
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName == "LI"){
         e.target.classList.toggle("checked");
         saveData();
+    
     }
     else if(e.target.tagName == "SPAN"){
         e.target.parentElement.remove();
@@ -35,7 +54,20 @@ function saveData(){
 }
 
 function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
+    if (localStorage.getItem("data")) {
+        listContainer.innerHTML = localStorage.getItem("data");
+    }
+    let editButtons = document.querySelectorAll("li button");
+    editButtons.forEach(button => {
+        button.onclick = function () {
+            let li = this.parentElement;
+            let newText = prompt("Edit your task:", li.firstChild.textContent);
+            if (newText !== null && newText.trim() !== '') {
+                li.firstChild.textContent = newText;
+                saveData();
+            }
+        };
+    });
 }
-
+// localStorage.clear();
 showTask();
